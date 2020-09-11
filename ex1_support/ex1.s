a76ef869909eb4ef8c949ab5357395683db44390
Tor Andre Haugdahl <thaugdahl@gmail.com>
2020-09-11 09:54:41 +0200

    Reset function

--- ex1_support/ex1.s
+++ ex1_support/ex1.s
@@ -82,7 +82,52 @@
 	      .type   _reset, %function
         .thumb_func
 _reset: 
-	      b .  // do nothing
+	// Set up aliases for constant-valued registers
+	GPIO_O .req R4
+	GPIO_I .req R5
+	GPIO .req R6
+
+	LDR GPIO_O, =GPIO_PA_BASE
+	LDR GPIO_I, =GPIO_PC_BASE
+	LDR GPIO, =GPIO_BASE
+
+	// Enable Clock
+	LDR R9, =CMU_BASE
+	LDR R7, [R9, #CMU_HFPERCLKEN0]
+	MOV R8, #1
+	LSL R8, R8, #CMU_HFPERCLKEN0_GPIO
+	ORR R7, R7, R8
+	STR R7, [R9, #CMU_HFPERCLKEN0]
+
+	// Low drive strength
+	MOV R7, =0x55555555
+	STR R7, [GPIO_O, #GPIO_MODEH]
+
+	BL led_test
+	LDR R7, =0xFF00
+	STR R7, [GPIO_O, #GPIO_DOUT]
+
+	// Set port C 0-7 to input
+	LDR R7, =0x33333333
+	STR R7, [GPIO_I, #GPIO_MODEL]
+	LDR R7, =0xFF
+	STR R7, [GPIO_I, #GPIO_DOUT]
+
+
+	.thumb_func
+main:
+	BL .
+
+	.thumb_func
+sleeper:
+	LDR R3, =0x30000
+sleeper_loop:
+	SUBS R3, #1
+	BNE sleeper_loop
+	BX lr
+
+
+    b .  // do nothing
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -91,14 +136,30 @@ _reset:
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
+
+		.thumb_func
+led_test:
+	PUSH {LR}
+	BL led_loop
+
+
+	.thumb_func
+led_loop:
+	PUSH {LR}
+	STR R7, [GPIO_O, #GPIO_DOUT]
+	BL wait
+	ROR R7, R8
+	SUBS R9, #1
+	BNE led_loop
+	POP{PC}
+
+
         .thumb_func
 gpio_handler:  
 
 	      b .  // do nothing
 	
 	/////////////////////////////////////////////////////////////////////////////
-	
         .thumb_func
 dummy_handler:  
         b .  // do nothing
-
