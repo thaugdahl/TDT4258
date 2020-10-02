6000a5b6e9a881df482946f5c6cd84107c0bddbb
Blackladder <blackladder0@gmail.com>
2020-10-02 10:51:49 +0200

    Added sine waveform and basic sound functions

--- ex2_support/dac.c
+++ ex2_support/dac.c
@@ -2,6 +2,10 @@
 #include <stdbool.h>
 
 #include "efm32gg.h"
+#include "sounds.h"
+
+int freq = 1;
+int i = 0;
 
 void setupDAC()
 {
@@ -14,7 +18,7 @@ void setupDAC()
 	 * 4. Write a continuous stream of samples to the DAC data registers, DAC0_CH0DATA and DAC0_CH1DATA, for example from a timer interrupt 
 	 */
 	/**
-	 * 5 - Prescale : 2^5 = 32 (if sinemode is enabled frequency will be )
+	 * 5 - Prescale : 2^5 = 32 (Clock divided by 32 (base is 14 MHz), if sinemode is enabled frequency will be )
 	 * 0 - reserved
 	 * 0 - 2 MSB reserved, 2 LSB - REFSEL = 0 (Internal 1.25 V bandgap reference)
 	 * 1 - OUTMODE = 1 (PIN - DAC output to pin enabled. DAC output to ADC and ACMP disabled)
@@ -24,4 +28,24 @@ void setupDAC()
 	*DAC0_CTRL = 0x50010;
 	*DAC0_CH0CTRL = 0x1;
 	*DAC0_CH1CTRL = 0x1;
+}
+
+/**TODO: finish comment
+ * 
+*/
+int period;
+void set_freq(int frequency){
+	if ((frequency < 0) || (frequency > 7)){
+		return -1;
+	}
+	freq = (1 << frequency);
+}
+
+/**TODO: finish comment
+ * timer interrupt will call this function
+*/
+void advance_sine(){
+	i = ++i % 256;
+	*DAC0_CH0DATA = sinewave[i*freq];
+	*DAC0_CH1DATA = sinewave[i*freq];
 }
