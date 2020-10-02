ed8e66e320fd9ff1f85dafb0da33de3dfb0247a3
Christian Steinsland <chrissteinsland@gmail.com>
2020-10-02 11:38:30 +0200

    Clear timer interrupt flag

--- ex2_support/interrupt_handlers.c
+++ ex2_support/interrupt_handlers.c
@@ -3,35 +3,38 @@
 
 #include "efm32gg.h"
 
-/*
+/**
  * TIMER1 interrupt handler 
  */
 void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 {
-	/*
-	 * TODO feed new samples to the DAC remember to clear the pending
-	 * interrupt by writing 1 to TIMER1_IFC 
+	/**
+	 * TODO feed new samples to the DAC 
+	 * Remember to clear the pending interrupt,
+	 * by writing 1 to TIMER1_IFC 
 	 */
+	
+	TIMER_IFC |= 1;
 	advance_sine();
 }
 
-/*
+/**
  * GPIO even pin interrupt handler 
  */
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler()
 {
-	/*
+	/**
 	 * TODO handle button pressed event, remember to clear pending
 	 * interrupt 
 	 */
 }
 
-/*
+/**
  * GPIO odd pin interrupt handler 
  */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler()
 {
-	/*
+	/**
 	 * TODO handle button pressed event, remember to clear pending
 	 * interrupt 
 	 */
